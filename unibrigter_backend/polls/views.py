ec6b57c02b595ebb966bde7ae09711447064220e
Kunliang <rocklee.au@foxmail.com>
2019-05-25 20:46:34 +0800

    half-done section 3

--- unibrigter_backend/polls/views.py
+++ unibrigter_backend/polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("First blood!")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s" % question_id)
